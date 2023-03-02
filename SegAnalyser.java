97c53022659dbfd61d0762a5bc8341b8411b39a2
belmella <sami.belmellat@etu.u-paris.fr>
2023-03-02 22:48:32 +0100

    <Changement de nom d'une class>

---
+++ SegAnalyser.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.Scanner;
+
+import Exceptions.ErrorLoadingFileException;
+
+public class SegAnalyser {
+    private File segFile ;
+    private int nbLocMale ;
+    private int nbLocFemale ;
+
+    private void reset () {
+        nbLocMale = 0 ;
+        nbLocFemale = 0 ; 
+        segFile = null ; 
+    }
+
+    public int getNbLocFemale() {
+        return nbLocFemale;
+    }
+
+    public int getNbLocMale() {
+        return nbLocMale;
+    }
+
+    private void loadFile (String path){
+        segFile = new File(path) ; 
+    }
+
+    public void analysis (String path) throws ErrorLoadingFileException {
+        reset();
+        loadFile(path);
+        Scanner sc ;
+        String line ; 
+        try {
+            sc = new Scanner(segFile) ; 
+        } catch (Exception e) {
+           throw new ErrorLoadingFileException() ;
+        }
+        sc.useDelimiter(";;") ; 
+        while(sc.hasNext()) {
+            line = sc.next() ; 
+            if (line.contains(" M ")) nbLocMale++ ;
+            if (line.contains(" F ")) nbLocFemale++ ; 
+        }
+        sc.close();
+    }
+    
+}
