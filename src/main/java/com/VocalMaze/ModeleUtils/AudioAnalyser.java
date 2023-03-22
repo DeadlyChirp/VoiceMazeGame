1d3440ffdb078bc058d950a5339da7e37b7af519
BELMELLAT sami <belmellatsamy@gmail.com>
2023-03-22 23:00:29 +0100

    <Modifs AudioAnalyser>

--- src/main/java/com/VocalMaze/ModeleUtils/AudioAnalyser.java
+++ src/main/java/com/VocalMaze/ModeleUtils/AudioAnalyser.java
@@ -5,21 +5,12 @@ import com.VocalMaze.ModeleUtils.AnalyseVocal.SegAnalyser;
 import com.VocalMaze.ModeleUtils.AnalyseVocal.Terminal;
 import com.VocalMaze.ModeleUtils.AnalyseVocal.TranscriberV3;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioSystem;
-import java.io.File;
-import java.util.Arrays;
-
-import static com.VocalMaze.ModeleUtils.AnalyseVocal.LiumUtils.executeForResult;
-
 public class AudioAnalyser {
-    private LiumUtils liumUtils ; 
     private Terminal terminal ;
     private SegAnalyser segAnalyser ;
     private TranscriberV3 transcriber ;
 
     public AudioAnalyser() {
-        liumUtils = new LiumUtils() ; 
         terminal = new Terminal() ;
         segAnalyser = new SegAnalyser() ; 
         transcriber = new TranscriberV3() ; 
@@ -29,7 +20,7 @@ public class AudioAnalyser {
         terminal.execCommand("/usr/bin/java -Xmx2024m -jar ./LIUM_SpkDiarization-8.4.1.jar "+
         "--finputDesc=audio16Khz2sphinx:sphinx,1:1:0:0:0:0,18,0:0:0:0 " +
         "--fInputMask=./src/main/java/com/VocalMaze/Records/Audio.wav " +
-        "--sOutputMask=/src/main/java/com/VocalMaze/Records/Audio.wav --doCEClustering Audio");
+        "--sOutputMask=/src/main/java/com/VocalMaze/Analysis/Audio.seg --doCEClustering Audio");
         try {
             segAnalyser.analysis("src/main/java/com/VocalMaze/Analysis/Audio.seg");
         } catch (Exception e) {
@@ -43,7 +34,6 @@ public class AudioAnalyser {
         return transcriber.transcription() ; 
     }
 
-
     public int[] analyse2() {
         try {
             LiumUtils.Result result = LiumUtils.executeForResult("./src/main/java/com/VocalMaze/Records/Audio.wav");
@@ -54,6 +44,5 @@ public class AudioAnalyser {
         }
         return new int[]{0, 0};
     }
-
 
 }
