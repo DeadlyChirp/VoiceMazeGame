ad9c7ab04c20bfed646cb5b386a315db90165311
BELMELLAT sami <belmellatsamy@gmail.com>
2023-04-06 20:10:31 +0200

    <ReOrganisation du code GameView>

--- src/main/java/com/VocalMaze/GameView.java
+++ src/main/java/com/VocalMaze/GameView.java
@@ -13,158 +13,169 @@ import java.io.IOException;
 import com.VocalMaze.ModeleUtils.Direction;
 
 public class GameView extends JPanel{
-    private Controller controller;
-    private boolean enDeplacement = false;
-    private BufferedImage[][] sprites;
-    private BufferedImage[][] porteLabyrinthe;
-    private int currentFrame = 0;
-    private long lastTime = 0;
-    private BufferedImage imagePorte;
-    private BufferedImage imagePassage;
-    private BufferedImage imageSprite;
-    private Direction dirAnim = Direction.BAS;
-    private int caseX, ancienCaseX;
-    private int caseY, ancienCaseY;
-    private int stepsAnim;
+    private Controller controller ; 
+    private LabyrintheView labyrintheView ; 
   
-
-    public GameView() throws IOException {
-        imageSprite = ImageIO.read(new File("src/main/java/com/VocalMaze/Images/professor_walk_cycle_no_hat.png"));
-        sprites = new BufferedImage[4][9];
-        imagePorte = ImageIO.read(new File("src/main/java/com/VocalMaze/Images/doors.png"));
-        imagePassage = ImageIO.read(new File("src/main/java/com/VocalMaze/Images/M484ShmupTileset1.png"));
-        porteLabyrinthe = new BufferedImage[30][30];
-        caseX = 0;
-        caseY = 0;
+    public GameView(String pseudo , int nbMaleTotal , int nbFemelleTotal) throws IOException {
+        controller = new Controller(new GameModel(pseudo, nbMaleTotal, nbFemelleTotal), this) ; 
+        labyrintheView = new LabyrintheView() ; 
+        labyrintheView.decoupeImage();
+        labyrintheView.setLocation(250, 100);
+        add(labyrintheView) ; 
+        labyrintheView.setVisible(true);
     }  
 
-    private void decoupeImage() {
-        for (int i = 0; i < sprites.length; i++) {
-            for (int j = 0; j < sprites[i].length; j++) {
-                sprites[i][j] = imageSprite.getSubimage(j*64, i*64, 64, 64);
-            }
-        }
+    public void movePlayer (Direction dir , int steps) {
+        labyrintheView.movePlayer(dir, steps);
+    }
 
-        for (int i = 0; i < porteLabyrinthe.length; i++) {
-            for (int j = 0; j < porteLabyrinthe[i].length; j++) {
-                if (controller.getOuvert(j, i)) {
-                    porteLabyrinthe[i][j] = imagePassage.getSubimage(1000, 70, 20, 20);
-                }else {
-                    porteLabyrinthe[i][j] = imagePorte.getSubimage(0, 0, 20, 20);
+    private class LabyrintheView extends JPanel {
+        private boolean enDeplacement ;
+        private BufferedImage[][] sprites;
+        private BufferedImage[][] porteLabyrinthe;
+        private int currentFrame ;
+        private long lastTime ;
+        private BufferedImage imagePorte;
+        private BufferedImage imagePassage;
+        private BufferedImage imageSprite;
+        private Direction dirAnim ;
+        private int caseX, ancienCaseX;
+        private int caseY, ancienCaseY;
+        private int stepsAnim;
+
+        public LabyrintheView () throws IOException{
+            imageSprite = ImageIO.read(new File("src/main/java/com/VocalMaze/Images/professor_walk_cycle_no_hat.png"));
+            sprites = new BufferedImage[4][9];
+            imagePorte = ImageIO.read(new File("src/main/java/com/VocalMaze/Images/doors.png"));
+            imagePassage = ImageIO.read(new File("src/main/java/com/VocalMaze/Images/M484ShmupTileset1.png"));
+            porteLabyrinthe = new BufferedImage[30][30];
+            caseX = 0;
+            caseY = 0;
+            currentFrame = 0 ; 
+            lastTime = 0 ; 
+            dirAnim = Direction.BAS ; 
+            enDeplacement = false ; 
+        }
+        
+        private void decoupeImage() {
+            for (int i = 0; i < sprites.length; i++) {
+                for (int j = 0; j < sprites[i].length; j++) {
+                    sprites[i][j] = imageSprite.getSubimage(j*64, i*64, 64, 64);
+                }
+            }
+
+    
+            for (int i = 0; i < porteLabyrinthe.length; i++) {
+                for (int j = 0; j < porteLabyrinthe[i].length; j++) {
+                    if (controller.getOuvert(j, i)) {
+                        porteLabyrinthe[i][j] = imagePassage.getSubimage(1000, 70, 20, 20);
+                    }else {
+                        porteLabyrinthe[i][j] = imagePorte.getSubimage(0, 0, 20, 20);
+                    }
+                }
+            }
+      }
+    
+        private void animateMovement () {
+            while(true) {
+                if (enDeplacement) {
+                    update();
+                    repaint();
+                    try {
+                        Thread.sleep(50);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }else{
+                    break ; 
                 }
             }
         }
-  }
-
-    private void animateMovement () {
-        while(true) {
-            if (enDeplacement) {
-                update();
-                repaint();
-                try {
-                    Thread.sleep(50);
-                } catch (Exception e) {
-                    e.printStackTrace();
+    
+        private void update() {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastTime > 500) {
+                if (enDeplacement) currentFrame++;
+                if (currentFrame >= sprites[3].length) {
+                    switch(dirAnim) {
+                        case DROITE : {
+                            caseX += currentFrame * stepsAnim;
+                            if (caseX + currentFrame * stepsAnim >= ancienCaseX + 20 * stepsAnim) enDeplacement = false;
+                            break;
+                        }
+    
+                        case GAUCHE : {
+                            caseX -= currentFrame * stepsAnim;
+                            if (caseX - currentFrame * stepsAnim <= ancienCaseX - 20 * stepsAnim) enDeplacement = false;
+                            break;
+                        }
+    
+                        case BAS : {
+                            caseY += currentFrame * stepsAnim;
+                            if (caseY + currentFrame * stepsAnim >= ancienCaseY + 20 * stepsAnim) enDeplacement = false;
+                            break;
+                        }
+    
+                        case HAUT : {
+                            caseY -= currentFrame * stepsAnim;
+                            if (caseY - currentFrame * stepsAnim <= ancienCaseY - 20 * stepsAnim) enDeplacement = false;
+                            break;
+                        }
+                    }
+                    currentFrame = 0;
+                    lastTime = currentTime;
                 }
-            }else{
-                break ; 
             }
         }
-    }
-
-    private void update() {
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - lastTime > 500) {
-            if (enDeplacement) currentFrame++;
-            if (currentFrame >= sprites[3].length) {
-                switch(dirAnim) {
-                    case DROITE : {
-                        caseX += currentFrame * stepsAnim;
-                        if (caseX + currentFrame * stepsAnim >= ancienCaseX + 20 * stepsAnim) enDeplacement = false;
-                        break;
-                    }
-
-                    case GAUCHE : {
-                        caseX -= currentFrame * stepsAnim;
-                        if (caseX - currentFrame * stepsAnim <= ancienCaseX - 20 * stepsAnim) enDeplacement = false;
-                        break;
-                    }
-
-                    case BAS : {
-                        caseY += currentFrame * stepsAnim;
-                        if (caseY + currentFrame * stepsAnim >= ancienCaseY + 20 * stepsAnim) enDeplacement = false;
-                        break;
-                    }
-
-                    case HAUT : {
-                        caseY -= currentFrame * stepsAnim;
-                        if (caseY - currentFrame * stepsAnim <= ancienCaseY - 20 * stepsAnim) enDeplacement = false;
-                        break;
-                    }
+    
+        public void movePlayer(Direction dir, int steps) {
+            enDeplacement = true;
+            dirAnim = dir;
+            stepsAnim = steps;
+            ancienCaseX = caseX;
+            ancienCaseY = caseY;
+            animateMovement();
+        }
+      
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            for (int i = 0; i < porteLabyrinthe.length; i++) {
+                for (int j = 0; j < porteLabyrinthe[i].length; j++) {
+                    g.drawImage(porteLabyrinthe[i][j], 20+i*20, 40+j*20, null);
                 }
-                currentFrame = 0;
-                lastTime = currentTime;
+            }
+            switch(dirAnim) {
+            case DROITE : {
+                if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX + currentFrame * stepsAnim, caseY, null);
+                else g.drawImage(sprites[dirAnim.ordinal()][0], caseX + currentFrame * stepsAnim, caseY, null);
+                break;
+            }
+            case GAUCHE : {
+                if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX - currentFrame * stepsAnim, caseY, null);
+                else g.drawImage(sprites[dirAnim.ordinal()][0], caseX - currentFrame * stepsAnim, caseY, null);
+                break;
+            }
+            case HAUT : {
+                if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX, caseY - currentFrame * stepsAnim, null);
+                else g.drawImage(sprites[dirAnim.ordinal()][0], caseX + currentFrame*2, caseY - currentFrame * stepsAnim, null);
+                break;
+            }
+            case BAS : {
+                if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX, caseY + currentFrame * stepsAnim, null);
+                else g.drawImage(sprites[dirAnim.ordinal()][0], caseX + currentFrame*2, caseY + currentFrame * stepsAnim, null);
+                break;
+            }
             }
         }
     }
-
-    public void movePlayer(Direction dir, int steps) {
-        enDeplacement = true;
-        dirAnim = dir;
-        stepsAnim = steps;
-        ancienCaseX = caseX;
-        ancienCaseY = caseY;
-        animateMovement();
-    }
-  
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        for (int i = 0; i < porteLabyrinthe.length; i++) {
-            for (int j = 0; j < porteLabyrinthe[i].length; j++) {
-                g.drawImage(porteLabyrinthe[i][j], 20+i*20, 40+j*20, null);
-            }
-        }
-        switch(dirAnim) {
-        case DROITE : {
-            if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX + currentFrame * stepsAnim, caseY, null);
-            else g.drawImage(sprites[dirAnim.ordinal()][0], caseX + currentFrame * stepsAnim, caseY, null);
-            break;
-        }
-        case GAUCHE : {
-            if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX - currentFrame * stepsAnim, caseY, null);
-            else g.drawImage(sprites[dirAnim.ordinal()][0], caseX - currentFrame * stepsAnim, caseY, null);
-            break;
-        }
-        case HAUT : {
-            if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX, caseY - currentFrame * stepsAnim, null);
-            else g.drawImage(sprites[dirAnim.ordinal()][0], caseX + currentFrame*2, caseY - currentFrame * stepsAnim, null);
-            break;
-        }
-        case BAS : {
-            if (enDeplacement) g.drawImage(sprites[dirAnim.ordinal()][currentFrame], caseX, caseY + currentFrame * stepsAnim, null);
-            else g.drawImage(sprites[dirAnim.ordinal()][0], caseX + currentFrame*2, caseY + currentFrame * stepsAnim, null);
-            break;
-        }
-        }
-}
 
   public static void main(String[] args) throws IOException {
     JFrame frame = new JFrame();
     frame.setPreferredSize(new Dimension(800, 800));
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    GameView gameView = new GameView();
-    gameView.controller = new Controller(new GameModel("test", 2, 2), gameView);
-    gameView.decoupeImage();
+    GameView gameView = new GameView("test" , 2 , 2);
     frame.add(gameView);
     frame.pack();
     frame.setVisible(true);
-    gameView.movePlayer(Direction.BAS, 5);
-
-    gameView.movePlayer(Direction.DROITE, 5);
-
-    gameView.movePlayer(Direction.DROITE, 5);
-
-    gameView.movePlayer(Direction.BAS , 10);
-    System.out.println("aaa");
   }
 }
